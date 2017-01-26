f4cbc64d9c87ab2b487e2c904188018e785e8f14
Gail Matsushima <gail.matsushima@gmail.com>
2017-01-25 23:57:55 -1000

    tabbing through and removing open from previous dropdown

--- Public/js/menu-navigation.js
+++ Public/js/menu-navigation.js
@@ -1,16 +1,11 @@
-$('li > a').keydown(function(e){
-
-var subMenuChildren = document.getElementsByTagName("li");
-var subMenu = $(this).next('ul');
-
-for (var i = 0; i < subMenuChildren.length; i++){
-  var lastDropdown = document.getElementsByClassName('lastDropdown');
-
-  if (e.which == 9 && subMenuChildren !== lastDropdown) {
+$('li>a').keydown(function(e) {
+  if (e.which == 9) {
+    //Get the submenu
+    var subMenu = $(this).next('ul');
     subMenu.addClass('open');
+    //Check if you're on the last subchild and close the menu
+    if ($(this).parent('li').is(':last-child:not(".menu>li>a")')) {
+      console.log($(this).parent('li').parent('.open').removeClass('open'));
+    }
   }
-  else{
-    subMenu.removeClass('open');
-  }
-}
 });
