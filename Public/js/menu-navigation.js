40e152713994f128c26f3934e4a3bea0402e2b60
Gail Matsushima <gail.matsushima@gmail.com>
2017-02-28 15:24:58 -1000

    added some notes

--- Public/js/menu-navigation.js
+++ Public/js/menu-navigation.js
@@ -2,10 +2,13 @@ $('li>a').keydown(function(e) {
   if (e.which == 9) {
     //Get the submenu
     var subMenu = $(this).next('ul');
+    //console.log('subMenu', subMenu);
     subMenu.addClass('open');
     //Check if you're on the last subchild and close the menu
+    console.log('hello', $(this).parent('li'));
     if ($(this).parent('li').is(':last-child:not(".menu>li>a")')) {
-      console.log($(this).parent('li').parent('.open').removeClass('open'));
+      //console.log($(this).parent('li'));
+      $(this).parent('li').parent('.open').removeClass('open');
     }
   }
 });
